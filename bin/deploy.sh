bc702c127c8676af11ca3bc01e9ea5d69008b240
Chen Gong <mychaint@gmail.com>
2018-07-15 12:58:08 +0800

    add scala module for spark streaming, add kafka module

---
+++ bin/deploy.sh
@@ -0,0 +1,7 @@
+WHITE='\033[1;37m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
+echo "${BLUE}[${GREEN}Build${BLUE}] ${WHITE}Start building this project."
+rm -rf dist
+mkdir dist
---
+++ kafka/brokers.sh
@@ -0,0 +1,1 @@
+sh zookeeper.sh localhost:2180 <<< "ls /brokers/ids"
