a05541003068e654bea8b55ad643efa12ab001e1
Chen Gong <mychaint@gmail.com>
2018-07-22 00:29:44 +0800

    update kafka producer brokers

--- bin/start-produce.sh
+++ bin/start-produce.sh
@@ -1,3 +1,8 @@
+WHITE='\033[1;37m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+
+echo "${BLUE}[${GREEN}Build${BLUE}] ${WHITE}Start building mock data producer."
 cd ../python/
 make build
 cd ../python-dist/
--- python/mock/producer.py
+++ python/mock/producer.py
@@ -6,23 +6,27 @@ from random import random, randint
 from kafka import KafkaProducer
 
 def produce_mock_orders_json(brokers, topic):
-    producer = KafkaProducer(bootstrap_servers=[brokers])
-    itemid = [(id, round(random() * randint(2, 100), 2)) for id in xrange(100000, 130000)]
-    orderid = 1000000000
+    producer = KafkaProducer(bootstrap_servers=brokers.split(","), retries=5)
+    shopid = [id for id in xrange(100000, 101200)]
+    itemid = [(id, round(random() * randint(2, 100), 2), shopid[id % 1200]) for id in xrange(100000, 130000)]
+    orderid = 1000000000000
+    region = ['SG', 'VN', 'TW','ID', 'TH','TW','ID','ID', 'TW','TW','TW','ID','TW', 'TH', 'MY', 'PH', 'ID']
     print "Start produce data into kafka %s, topic %s ..."  % (brokers, topic)
     while True:
-        item = itemid[randint(0, len(itemid))]
+        item = itemid[randint(0, len(itemid)-1)]
         record = {
             "itemid": str(item[0]),
             "price": str(item[1]),
+            "shopid": str(item[2]),
             "count": randint(1, 100),
             "timestamp": "%s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            "region": "SG",
+            "region": region[randint(0,len(region)-1)],
             "orderid": orderid
         }
+        print record
         orderid += 1
         producer.send(topic, json.dumps(record))
-        time.sleep(randint(0, 1000) / 1000.0)
+        time.sleep(randint(50, 600) / 1000.0)
 
 if __name__ == '__main__':
     conf = {} 
@@ -32,4 +36,4 @@ if __name__ == '__main__':
         key = line.split("=")[0]
         value = line.split("=")[1]
         conf.update({key: value})
-    produce_mock_orders_json(conf['brokers'], conf['topic'])
+    produce_mock_orders_json(conf.get('brokers', ''), conf.get('topic', ''))
